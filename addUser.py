dbf12bc6cba421f435b9e68e463afeab2af320b0
Sahrutha Reddy <107304838+Sahruthak@users.noreply.github.com>
2023-11-23 21:07:59 +0530

    Update addUser.py

--- addUser.py
+++ addUser.py
@@ -3,7 +3,6 @@ import os
 import faceDetect as fd
 import numpy as np
 
-
 face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 data_path='training-data'
 path='usernames/users.txt'
